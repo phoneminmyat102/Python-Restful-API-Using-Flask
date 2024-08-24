f10981912015ad2fb9f4043514d0306a4b2ab876
phoneminmyat102 <phoneminmyat102115@gmail.com>
2024-08-24 23:47:13 +0700

    project setup and install dependencies, database setup

---
+++ api.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+api = Api(app)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(name = {self.name}, email = {self.email})"
+
+@app.route('/')
+def home():
+    return '<h1>THIS IS HOME PAGE</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
