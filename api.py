133746da74de508526541404ff80a3406b1c6e2b
phoneminmyat102 <phoneminmyat102115@gmail.com>
2024-08-25 17:53:33 +0700

    completion of building restful api

--- api.py
+++ api.py
@@ -15,6 +15,64 @@ class UserModel(db.Model):
     def __repr__(self) -> str:
         return f"User(name = {self.name}, email = {self.email})"
 
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, required=True, help="Must Enter Your Name")
+user_args.add_argument('email', type=str, required=True, help="Must Enter Your Email")
+
+
+userFields = {
+    'id':fields.Integer,
+    'name':fields.String,
+    'email':fields.String,
+}
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+    
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args['name'], email=args['email'])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+
+class User(Resource):
+    @marshal_with(userFields)
+    def get(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404)
+        return user
+    
+    @marshal_with(userFields)
+    def patch(self, id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404)
+        user.name = args['name']
+        user.email = args['email']
+        db.session.commit()
+        return user
+    
+    @marshal_with(userFields)
+    def delete(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404)
+        db.session.delete(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users
+    
+api.add_resource(Users, '/api/users/')
+api.add_resource(User, '/api/users/<int:id>')
+
 @app.route('/')
 def home():
     return '<h1>THIS IS HOME PAGE</h1>'
